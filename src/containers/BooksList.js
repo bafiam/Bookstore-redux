a5f6cb1459d26688f92153b9bb3920c152cf1714
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-06-22 22:24:49 +0100

    build reducers and action for redux store

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
 
-export default class BooksList extends Component {
-  render() {
-    return (
-      <div>
-        <h3>book list</h3>
-      </div>
-    )
-  }
-}
+const BooksList = props => {
+  const { books } = props;
+  const bookList = books.length ? (
+    books.map((book) => {
+      return (
+        <Book book={book} key={book.id} />
+      );
+    })
+  ) : (
+      <div>Oops! No Book in the store!</div>
+    );
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookList}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    books: state.books,
+  };
+};
+
+export default connect(mapStateToProps)(BooksList);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { randomID } from './actions/index';
+import rootReducer from './reducers/index';
+
+const initialState = [
+  {
+    id: randomID(),
+    title: 'How to be a better Programmer',
+    category: 'Learning',
+  },
+  {
+    id: randomID(),
+    title: 'The myth about World War 2',
+    category: 'History',
+  },
+  {
+    id: randomID(),
+    title: 'The Moonlight Boy',
+    category: 'Action',
+  },
+  {
+    id: randomID(),
+    title: 'The Boy and the Giant',
+    category: 'Kids',
+
+  },
+];
+
+const store = createStore(rootReducer, {
+  books: initialState,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
